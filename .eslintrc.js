d3ce1e547b6751ca0c118d0531d0292bf06c9fa9
Joshua Wiens <joshuaw@easymetrics.com>
2017-12-17 06:04:55 -0600

    chore: Update to defaults beta.2

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,11 +7,9 @@ module.exports = {
       'error',
       { singleQuote: true, trailingComma: 'es5', arrowParens: 'always' },
     ],
-    "prefer-destructuring": 1,
     "prefer-rest-params": 0,
     "class-methods-use-this": 1,
     "no-plusplus": 1,
     "consistent-return": 0,
-    "no-param-reassign": 0
   },
 };
